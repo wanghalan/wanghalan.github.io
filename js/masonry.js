a840b54ff0803a30f6a9a08cfab011780f93ea94
alan <ahw9f@virginia.edu>
2023-11-25 23:32:43 -0500

    patch: saving work to upgrade to masonry

---
+++ js/masonry.js
@@ -0,0 +1,16 @@
+// external js: masonry.pkgd.js, imagesloaded.pkgd.js
+import Masonry from "https://unpkg.com/masonry-layout@4/dist/masonry.pkgd.min.js";
+
+// init Masonry
+var grid = document.querySelector('.grid');
+
+var msnry = new Masonry( grid, {
+  itemSelector: '.grid-item',
+  columnWidth: '.grid-sizer',
+  percentPosition: true
+});
+
+imagesLoaded( grid ).on( 'progress', function() {
+  // layout Masonry after each image loads
+  msnry.layout();
+});
